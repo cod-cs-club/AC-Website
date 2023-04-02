64c5548b89dcafef243e3eefce48d6499338e7d3
Sebastian Przybylski <generalg2003@gmail.com>
2023-04-01 19:52:11 -0500

    Added primative image slides

--- components/ImageCarousel.jsx
+++ components/ImageCarousel.jsx
@@ -1,20 +1,55 @@
+/**
+ * 'useStat' is a hook from React. It allows us to set that state of a element. It is defined as an
+ * array of two indexes, the first describing the current value, the second being a setter of a value.
+ * The parameter with the useState defines the intial value. In other words:
+ * 
+ * const [something, setSomething] = useState(initialValue)
+ * 
+ * (If you're confused, I highly reccomend you skim over the official React documentation on
+ * useState here: https://react.dev/reference/react/useState)
+ * 
+ * In this component, which iterates through the array 'slides' (a prop accepted in index.js),
+ * the buttons in the return statement determine whether to increase or decrease currentSlideIndex
+ * by 1, corresponding to the current displayed index of the slides array. In the function
+ * 'nextImg()', The variable 'isThisTheFinalSlide' determines whether the value of currentSlideIndex 
+ * is equal to the final index of the array (arrayLength - 1). 'newIndex', therefore, uses a ternary 
+ * expression to determine whether the new index should be set to 0 (the beginning of the array) or 
+ * to increment to the next element whether if the current index is the final slide or not 
+ * (as determined by the isThisTheFinalSlide variable). Lastly, setSlideIndex (the 2nd element in 
+ * our useState array) sets the value of currentSlideIndex to newIndex. 
+ * 
+ * 'prevImg()' works much in  the same way, only depreciating the value of currentSlideIndex instead 
+ * of raising it.
+ */
 import Image from 'next/image'
+import { useState } from 'react';
 
-export default function ImageCarousel(){
+export default function ImageCarousel({slides}){
+    let arrayLength = slides.length;
+    const [currentSlideIndex, setSlideIndex] = useState(0);
+    function nextImg(){
+        const isThisTheFinalSlide = currentSlideIndex == arrayLength - 1;
+        const newIndex = isThisTheFinalSlide ? 0 : currentSlideIndex + 1;
+        setSlideIndex(newIndex);
+    }
+
+    function prevImg(){
+        const isThisTheFirstSlide = currentSlideIndex == 0
+        const newIndex = isThisTheFirstSlide ? arrayLength - 1 : currentSlideIndex - 1;
+        setSlideIndex(newIndex);
+    }
+
+
     return(
         <>
-        <div id="image-carousel-container">
-            <div className="homepage-images">
-                <Image src="/Company Images/BasementUnit1.png" width="200" height="200"/>
+        <div id="sliderContainer">
+
+            <div>
+                <Image src={slides[currentSlideIndex].src} width="300" height="300" />
             </div>
+            <button onClick={() => nextImg()}>Next</button>
+            <button onClick={() => prevImg()}>Back</button>
         </div>
         </>
     )
 }
-/*
-//JS files can't be imported using a simple <script> tag in JSX, so I'm temporarily commenting these 
-out until I can get outer JS files properly rendered in a component. The JS code is currently at
-'static\imageCarousel.js', and will be referenced in this component 
-<a className="prev" onClick={plusSlides(-1)}>&#10094;</a>
-<a className="next" onClick={plusSlides(1)}>&#10095;</a>
-*/
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,12 +3,21 @@ import Footer from 'components/Footer.jsx'
 import ImageCarousel from 'components/ImageCarousel.jsx'
 import Header from 'components/Header.jsx'
 
+const sliderImages = [
+  {name: "img1", src:"/AC-unit.jpg"},
+  {name: "img2", src:"/BasementUnit2.png"},
+  {name: "img3", src:"/BasementUnit1.png"},
+  {name: "img4", src:"/Closet-AC.jpg"},
+  {name: "img5", src:"/Humidifier1.jpg"},
+  {name: "img6", src:"/Humidifier2.jpg"}
+]
+
 export default function Home() {
   return (
     <>
     <Header />
     <NavBar />
-    <ImageCarousel />
+      <ImageCarousel slides={sliderImages}/>
     <Footer />
     </>
   )
