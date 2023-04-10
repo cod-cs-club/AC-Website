cdbfbcc23ada0f57c41115695dc36717e395d0e7
unknown <jvgomz3@gmail.com>
2023-04-10 00:15:50 -0500

    changes to image dimensions

--- components/CarriersGrid.jsx
+++ components/CarriersGrid.jsx
@@ -4,18 +4,18 @@ export default function CarriersGrid(){
     return (
         <>
         <div id="logos-container">
-            <Image src="/Carrier Images/Amana.png" width="110" height="110" />
-            <Image src="/Carrier Images/AmericanStandard.png" width="110" height="110" />
-            <Image src="/Carrier Images/Bryant.png" width="110" height="110" />
-            <Image src="/Carrier Images/Carrier.png" width="110" height="110" />
-            <Image src="/Carrier Images/Coleman.png" width="110" height="110" />
-            <Image src="/Carrier Images/Goodman.png" width="110" height="110" />
-            <Image src="/Carrier Images/Lennox.png" width="110" height="110" />
-            <Image src="/Carrier Images/Rheem.png" width="110" height="110" />
-            <Image src="/Carrier Images/Ruud.png" width="110" height="110" />
-            <Image src="/Carrier Images/Trane.png" width="110" height="110" />
-            <Image src="/Carrier Images/Westinghouse.png" width="110" height="110" />
-            <Image src="/Carrier Images/York.png" width="110" height="110" />
+            <Image src="/Carrier Images/Amana.png" width="130" height="130" />
+            <Image src="/Carrier Images/AmericanStandard.png" width="130" height="130" />
+            <Image src="/Carrier Images/Bryant.png" width="130" height="130" />
+            <Image src="/Carrier Images/Carrier.png" width="130" height="130" />
+            <Image src="/Carrier Images/Coleman.png" width="130" height="130" />
+            <Image src="/Carrier Images/Goodman.png" width="130" height="130" />
+            <Image src="/Carrier Images/Lennox.png" width="130" height="130" />
+            <Image src="/Carrier Images/Rheem.png" width="130" height="130" />
+            <Image src="/Carrier Images/Ruud.png" width="130" height="130" />
+            <Image src="/Carrier Images/Trane.png" width="130" height="130" />
+            <Image src="/Carrier Images/Westinghouse.png" width="130" height="130" />
+            <Image src="/Carrier Images/York.png" width="130" height="130" />
         </div>
         </>
     )
--- components/ServicesGrid.jsx
+++ components/ServicesGrid.jsx
@@ -4,14 +4,15 @@ export default function ServicesGrid(){
     return(
         <>
         <div id="service-images-container">
-            <Image src="/Company Images/AC-Unit.jpg" width="230" height="250"/>
-            <Image src="/Company Images/BasementUnit1.png" width="230" height="250" />
-            <Image src="/Company Images/BasementUnit2.png" width="230" height="250" />
-            <Image src="/Company Images/Closet-AC.jpg" width="230" height="250" />
-            <Image src="/Company Images/Humidifier1.jpg" width="230" height="250" />
-            <Image src="/Company Images/Humidifier2.jpg" width="230" height="250" />
-            <Image src="/Company Images/OutdoorUnits.jpg" width="230" height="250" />
-            <Image src="/Company Images/RoofUnit.jpg" width="230" height="250" />
+            <Image src="/Company Images/AC-Unit.jpg" width="425" height="425"/>
+            <Image src="/Company Images/BasementUnit1.png" width="425" height="425" />
+            <Image src="/Company Images/BasementUnit2.png" width="425" height="425" />
+            <Image src="/Company Images/Closet-AC.jpg" width="425" height="425" />
+            <Image src="/Company Images/Humidifier1.jpg" width="425" height="425" />
+            <Image src="/Company Images/Humidifier2.jpg" width="425" height="425" />
+            <Image src="/Company Images/OutdoorUnits.jpg" width="425" height="425" />
+            <Image src="/Company Images/RoofUnit.jpg" width="425" height="425" />
+            <Image src="/Company Images/WaterHeater1.jpg" width="425" height="425" />
         </div>
         </>
     )
