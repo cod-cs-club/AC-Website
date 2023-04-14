246a6f5aa9cc16e497047330049d7e495306188c
unknown <jvgomz3@gmail.com>
2023-04-14 01:57:57 -0500

    updated quote page layout

--- src/pages/Quote.jsx
+++ src/pages/Quote.jsx
@@ -6,18 +6,42 @@ export default function QuotePage(){
         <>
         <NavBar />
         <div id="quoteText">
-            <div>
+            <div id="quote-heading">
                 <h1>Got a problem? We can fix it!</h1>
                 <h2>Send us an email & we'll get back to you as soon as we can</h2>
             </div>
             <div>
                 <form>
-                    <h2>Name:</h2>
-                    <input type="text"></input>
-                    <h2>Email:</h2>
-                    <input type="text"></input>
-                    <h2>Message:</h2>
-                    <input type="text"></input>
+                    <fieldset class="form-box1">
+                        <label id="label-firstname" for="firstname">
+                            <span>First Name</span>
+                        </label>
+                        <input id="firstname" type="text" name="firstname" placeholder="First Name" inputmode="text" value="" autocomplete="given-name" required></input>
+                    </fieldset>
+
+                    <fieldset class="form-box2">
+                        <label id="label-lastname" for="lastname">
+                            <span>Last Name</span>
+                        </label>
+                        <input id="lastname" type="text" name="lastname" placeholder="Last Name" inputmode="text" value="" required></input>
+                    </fieldset>
+
+                    <fieldset id="form-box3">
+                        <label id="label-email" for="email">
+                            <span>Email</span>
+                        </label>
+                        <input id="email" type="text" name="email" placeholder="Email Address" inputmode="text" value="" required></input>
+                    </fieldset>
+
+                    <fieldset id="form-box4">
+                        <label id="label-msg" for="msg">
+                            <span>Message</span>
+                        </label>
+                        <input id="msg" type="text" name="msg" placeholder="Enter your message" inputmode="text" value="" required></input>
+                    </fieldset>
+                    <div class="submit-button">
+                        <input class="submit" type="submit" value="Submit"></input>
+                    </div>
                 </form>
             </div>
         </div>
