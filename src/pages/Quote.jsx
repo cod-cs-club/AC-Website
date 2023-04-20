44c5c2cc49fb5125673fde95fd3854c344b155d1
tyson <tysonwargin3@gmail.com>
2023-04-20 18:19:55 -0500

    Added form backend (format/test)

--- src/pages/Quote.jsx
+++ src/pages/Quote.jsx
@@ -1,49 +1,65 @@
+import { useState } from 'react'
+import { sendContactForm } from '/lib/api';
 
-export default function QuotePage(){
-    return (
-        <>
-        <div id="quoteText">
-            <div id="quote-heading">
-                <h1>Got a problem? We can fix it!</h1>
-                <h2>Send us an email & we'll get back to you as soon as we can</h2>
-            </div>
-            <div>
-                <form>
-
-                <fieldset className="form-box1">
-                    <label id="label-email" htmlFor="subject">
-                        <span>Subject</span>
-                    </label>
-                    <input id="subject" type="subject" name="subject" placeholder="Subject" required></input>
-                </fieldset>
-
-                <fieldset className="form-box2">
-                    <label id="label-firstname" htmlFor="name">
-                        <span>Name</span>
-                    </label>
-                    <input id="firstname" type="text" name="name" placeholder="name" inputmode="text" autocomplete="given-name" required></input>
-                </fieldset>
-
-                    <fieldset className="form-box3">
-                        <label id="label-email" htmlFor="email">
-                            <span>Email</span>
-                        </label>
-                        <input id="email" type="email" name="email" placeholder="Email Address" required></input>
-                </fieldset>
-
-                <fieldset id="form-box4">
-                    <label id="label-msg" for="msg">
-                        <span>Details</span>
-                    </label>
-                    <textarea id="msg" type="text" name="message" placeholder="Additional Details" inputmode="text" required></textarea>
-                </fieldset>
-
-                    <div className="submit-button">
-                        <input className="submit" type="submit" value="Submit"></input>
-                    </div>
-                </form>
-            </div>
-        </div>
-        </>
-    )
+const initValues = {
+  name:"", 
+  subject:"",
+  email:"",
+  message:""
 }
+
+const initState = {values:initValues}
+
+export default function QuotePage() {
+ 
+  const [state, setState] = useState(initState);
+   
+  const {values} = state;
+
+  const handleChange = ({target}) => 
+    setState((prev) => ({
+      ...prev, 
+      values:{
+        ...prev.values,
+      
+        [target.name]: target.value
+      }
+  }));
+
+  const onSubmit = async () => {
+    try {
+      await sendContactForm(values);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <>
+       <form>
+        <fieldset>
+          <div>
+            <label htmlFor="name">Name:</label>
+              <input type="text" id="name" name="name" value={values.name} placeholder="name" onChange={handleChange} required/>
+          </div>
+          <div>
+          <label htmlFor="subject">Subject:</label>
+            <input type="text" id="sub" name="subject" value={values.subject} placeholder="Subject" onChange={handleChange} required/>
+          </div>
+          <div>
+          <label htmlFor="email">Email:</label>
+            <input type="email" id="email" name="email" value={values.email} placeholder="Email Address" onChange={handleChange} required/>
+          </div>
+          <div>
+          <label htmlFor="message">Details:</label>
+            <textarea type="text" id="message" name="message" value={values.message} placeholder="Additional Details" onChange={handleChange} required/>
+          </div>
+          <div>
+            <button className="submit" onClick={onSubmit} disabled={!values.name || !values.subject || !values.email || !values.message}>Submit</button>
+          </div>
+        </fieldset>
+      </form>
+    </>
+  )
+}
+
