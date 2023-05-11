4607276862d2a561409342a8bf8430cdff88d607
unknown <jvgomz3@gmail.com>
2023-05-11 01:27:50 -0500

    Added show more/show less functionality to service panes

--- components/ServicePane.jsx
+++ components/ServicePane.jsx
@@ -1,13 +1,24 @@
 import Image from 'next/image';
+import React, { useState } from 'react';
 
-const ServicePane = ({paneImage, Header, bodyText, Imagealt}) => (
-    <div id="serviceImagePane">
-        <div id="serviceImage">
-            <Image src={paneImage} alt={Imagealt} width="450" height="400"/>
+const ServicePane = ({paneImage, Header, bodyText, Imagealt}) => {
+    const [showFullText, setShowFullText] = useState(false);
+    
+    function toggleText() {
+        setShowFullText(!showFullText);
+    }
+
+    const shortText = bodyText.slice(0,100);
+
+    return (
+        <div id="serviceImagePane">
+            <div id="serviceImage">
+                <Image src={paneImage} alt={Imagealt} width="450" height="400"/>
+            </div>
+        <h4>{Header}</h4>
+        <p>{showFullText ? bodyText : shortText}</p>
+        <button onClick={toggleText}>{showFullText ? 'Show Less' : 'Show More'}</button>
         </div>
-    <h4>{Header}</h4>
-    <span>{bodyText}</span>
-    </div>
-);
-
+    );
+};
 export default ServicePane;
