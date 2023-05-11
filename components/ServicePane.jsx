2c32af778fe491817378347095aa1414627b651f
unknown <jvgomz3@gmail.com>
2023-05-11 15:54:43 -0500

    new service pane layout

--- components/ServicePane.jsx
+++ components/ServicePane.jsx
@@ -1,5 +1,7 @@
 import Image from 'next/image';
 import React, { useState } from 'react';
+import { BiUpArrow, BiDownArrow } from 'react-icons/bi';
+
 
 const ServicePane = ({paneImage, Header, bodyText, Imagealt}) => {
     const [showFullText, setShowFullText] = useState(false);
@@ -9,16 +11,28 @@ const ServicePane = ({paneImage, Header, bodyText, Imagealt}) => {
     }
 
     //Choose how many characters you want to display before text is cut off
-    const shortText = bodyText.slice(0,100);
+    const shortText = bodyText.slice(0,0);
 
     return (
         <div id="serviceImagePane">
+            <h4>{Header}</h4>
             <div id="serviceImage">
                 <Image src={paneImage} alt={Imagealt} width="450" height="400"/>
             </div>
-        <h4>{Header}</h4>
+            <button onClick={toggleText}>
+                {showFullText ? (
+                    <>
+                        &nbsp;Show Less
+                        <BiUpArrow />
+                    </>
+                ) : (
+                    <>
+                        &nbsp;Show More
+                        <BiDownArrow />
+                    </>
+                )}
+            </button>
         <span>{showFullText ? bodyText : shortText}</span>
-        <button onClick={toggleText}>{showFullText ? 'Show Less' : 'Show More'}</button>
         </div>
     );
 };
